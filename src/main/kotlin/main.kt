cf83b8f196acfb9f39364f02c1c0a31e1adbca29
Joel Jorge Nunes Vázquez <72249166+jnunesvazquez@users.noreply.github.com>
2021-06-07 18:17:39 +0200

    Ejercicio 2 terminado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,11 @@
+import kotlin.random.Random
+
 fun main() {
     //Ejercicio 1
     for (numero in Ejercicio_1.array){
         print("$numero, ")
     }
+    println()
+    //Ejercicio 2
+    print(Ejercicio_2.showArray())
 }
